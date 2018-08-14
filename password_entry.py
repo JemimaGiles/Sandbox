b3db09d26f098e2f878e0ff1e6ffe4b08e5e4389
JemimaGiles <jemima.giles@my.jcu.edu.au>
2018-08-14 14:48:10 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 """Jemima Giles"""
+
+MIN_LENGTH = 6
+password = input("Enter Password: ")
+while len(password) < MIN_LENGTH:
+    print("Password is too short")
+    password = input("Enter Password: ")
+for char in password:
+    print("*", end=' ')
